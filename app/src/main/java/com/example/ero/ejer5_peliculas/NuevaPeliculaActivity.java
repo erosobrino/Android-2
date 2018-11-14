4fb28c003d1447987e10aaa84ac8466877580f21
erosobrino <ero.sobrino.dorado@gmail.com>
2018-11-14 10:47:49 +0100

    2

--- app/src/main/java/com/example/ero/ejer5_peliculas/NuevaPeliculaActivity.java
+++ app/src/main/java/com/example/ero/ejer5_peliculas/NuevaPeliculaActivity.java
@@ -9,5 +9,7 @@ public class NuevaPeliculaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nueva_pelicula);
+
+        Integer it=2;
     }
 }
